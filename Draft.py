c27e494de9f4d4697b2a5f5c7cb32a14e991223b
Aurik Anjum <109861996+Aurik011@users.noreply.github.com>
2022-10-29 19:43:38 +0600

    Python M1 Macbook file orw

--- Draft.py
+++ Draft.py
@@ -9,3 +9,14 @@ print(gaz)
 print("Hello")
 a = "Georgeo"
 print(a)  #prints contents of a
+
+# open read write
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile3.txt", "r")
+print(f.read())
+
+
